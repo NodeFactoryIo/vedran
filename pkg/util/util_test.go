0f43ed1b5616be16f0ea174ea350f4a836c2dc15
Mak Muftic <mak@nodefactory.io>
2020-09-25 15:39:40 +0200

    Expand tests for util

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -3,6 +3,7 @@ package util
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -14,25 +15,92 @@ type testRequest struct {
 }
 
 func TestDecodeJSONBody(t *testing.T) {
+
+	validTestRequest := testRequest{Id: "1"}
+	validTestRequestBody, _ := json.Marshal(validTestRequest)
+
 	tests := []struct {
 		name        string
 		contentType string
 		request     testRequest
 		err         MalformedRequest
+		bodyContent string
 		success     bool
 	}{
-		{name: "Valid body", contentType: "application/json", request: testRequest{Id: "1"}, err: MalformedRequest{
-				// empty error
-		}, success: true},
-		{name: "Valid body", contentType: "txt", request: testRequest{Id: "1"}, err: MalformedRequest{
-			Status: http.StatusUnsupportedMediaType,
-			Msg:    "Content-Type header is not application/json",
-		}, success: false},
+		{
+			name:        "body successfully decoded",
+			contentType: "application/json",
+			request:     validTestRequest,
+			err:         MalformedRequest{},
+			bodyContent: string(validTestRequestBody),
+			success:     true,
+		},
+		{
+			name:        "wrong Content-Type header",
+			contentType: "txt",
+			request:     validTestRequest,
+			err: MalformedRequest{
+				Status: http.StatusUnsupportedMediaType,
+				Msg:    "Content-Type header is not application/json",
+			},
+			bodyContent: string(validTestRequestBody),
+			success:     false,
+		},
+		{
+			name:        "unknown field in json body content",
+			contentType: "application/json",
+			request:     validTestRequest,
+			err: MalformedRequest{
+				Status: http.StatusBadRequest,
+				Msg:    "Request body contains unknown field \"prop\"",
+			},
+			bodyContent: "{\"prop\":\"value\"}",
+			success:     false,
+		},
+		{
+			name:        "baldy-formed JSON in json body content",
+			contentType: "application/json",
+			request:     validTestRequest,
+			err: MalformedRequest{
+				Status: http.StatusBadRequest,
+				Msg:    "Request body contains badly-formed JSON (at position 2)",
+			},
+			bodyContent: "{prop:\"value\"}",
+			success:     false,
+		},
+		{
+			name:        "empty json body content",
+			contentType: "application/json",
+			request:     validTestRequest,
+			err: MalformedRequest{
+				Status: http.StatusBadRequest,
+				Msg:    "Request body must not be empty",
+			},
+			bodyContent: "",
+			success:     false,
+		},
+		{
+			name:        "invalid value for field in json body content",
+			contentType: "application/json",
+			request:     validTestRequest,
+			err: MalformedRequest{
+				Status: http.StatusBadRequest,
+				Msg:    "Request body contains an invalid value for the \"id\" field (at position 8)",
+			},
+			bodyContent: "{\"id\":10}",
+			success:     false,
+		},
 	}
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			rb, _ := json.Marshal(test.request)
-			req, _ := http.NewRequest("POST", "/api/v1/", bytes.NewReader(rb))
+		var testPrefix string
+		if test.success {
+			testPrefix = "Valid request"
+		} else {
+			testPrefix = "Invalid request"
+		}
+		t.Run(fmt.Sprintf("%s %s", testPrefix, test.name), func(t *testing.T) {
+
+			req, _ := http.NewRequest("POST", "/api/v1/", bytes.NewReader([]byte(test.bodyContent)))
 			req.Header.Set("Content-Type", test.contentType)
 			rr := httptest.NewRecorder()
 			var parsedRequest testRequest
@@ -41,13 +109,12 @@ func TestDecodeJSONBody(t *testing.T) {
 
 			if test.success {
 				assert.Equal(t, err, nil)
-				assert.Equal(t, parsedRequest, test.request)
+				assert.Equal(t, test.request, parsedRequest)
 			} else {
 				assert.Error(t, err)
 				assert.EqualError(t, err, test.err.Msg)
-				assert.Equal(t, parsedRequest, testRequest{})
+				assert.Equal(t, testRequest{}, parsedRequest)
 			}
-
 		})
 	}
 }
