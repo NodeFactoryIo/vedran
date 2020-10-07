a669d4b877fe9cf1ce6392319bcf08f8e40e618e
Matija Petrunic <matija.petrunic@fer.hr>
2020-10-07 13:58:02 +0200

    Reward and penalize node depending on rpc response result

--- internal/repositories/node.go
+++ internal/repositories/node.go
@@ -1,6 +1,9 @@
 package repositories
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/NodeFactoryIo/vedran/internal/models"
 	"github.com/asdine/storm/v3"
 	"github.com/asdine/storm/v3/q"
@@ -38,9 +41,41 @@ func (r *NodeRepo) IsNodeWhitelisted(ID string) (bool, error) {
 	return isWhitelisted, err
 }
 
-func (r *NodeRepo) GetActiveNodes() (*[]models.Node, error) {
+func (r *NodeRepo) getRandomNodes() (*[]models.Node, error) {
 	var nodes []models.Node
+
 	q := r.db.Select(q.Lte("Cooldown", 0))
 	err := q.Find(&nodes)
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(nodes), func(i, j int) { nodes[i], nodes[j] = nodes[j], nodes[i] })
+
 	return &nodes, err
+}
+
+func (r *NodeRepo) getRoundRobinNodes() (*[]models.Node, error) {
+	var nodes []models.Node
+
+	q := r.db.Select(q.Lte("Cooldown", 0))
+	err := q.OrderBy("LastUsed").Reverse().Find(&nodes)
+
+	return &nodes, err
+}
+
+func (r *NodeRepo) GetActiveNodes(selection string) (*[]models.Node, error) {
+	if selection == "round-robin" {
+		return r.getRoundRobinNodes()
+	}
+
+	return r.getRandomNodes()
+}
+
+func (r *NodeRepo) PenalizeNode(node *models.Node) error {
+	node.LastUsed = time.Now()
+	return r.db.Update(node)
+}
+
+func (r *NodeRepo) RewardNode(node *models.Node) error {
+	node.LastUsed = time.Now()
+	return r.db.Update(node)
 }
