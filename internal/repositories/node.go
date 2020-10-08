63e02ea5da1fa840213e702fc244aaf68ec3d2d1
Matija Petrunic <matija.petrunic@fer.hr>
2020-10-08 15:47:59 +0200

    Keep active nodes in memory

--- internal/repositories/node.go
+++ internal/repositories/node.go
@@ -1,23 +1,35 @@
 package repositories
 
 import (
+	"fmt"
 	"math/rand"
+	"sort"
 	"time"
 
 	"github.com/NodeFactoryIo/vedran/internal/models"
 	"github.com/asdine/storm/v3"
-	"github.com/asdine/storm/v3/q"
 	log "github.com/sirupsen/logrus"
 )
+
+var memoryNodes []models.Node
 
 type NodeRepo struct {
 	db *storm.DB
 }
 
-func NewNodeRepo(db *storm.DB) *NodeRepo {
-	return &NodeRepo{
+func NewNodeRepo(db *storm.DB) (*NodeRepo, error) {
+	memoryNodes = make([]models.Node, 0)
+	repo := NodeRepo{
 		db: db,
 	}
+
+	nodes, err := repo.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	memoryNodes = *nodes
+	return &repo, nil
 }
 
 func (r *NodeRepo) FindByID(ID string) (*models.Node, error) {
@@ -27,6 +39,8 @@ func (r *NodeRepo) FindByID(ID string) (*models.Node, error) {
 }
 
 func (r *NodeRepo) Save(node *models.Node) error {
+	r.AddNodeToActive(node)
+
 	return r.db.Save(node)
 }
 
@@ -42,28 +56,24 @@ func (r *NodeRepo) IsNodeWhitelisted(ID string) (bool, error) {
 	return isWhitelisted, err
 }
 
-func (r *NodeRepo) getRandomNodes() (*[]models.Node, error) {
-	var nodes []models.Node
-
-	q := r.db.Select(q.Lte("Cooldown", 0))
-	err := q.Find(&nodes)
-
+func (r *NodeRepo) getRandomNodes() *[]models.Node {
 	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(nodes), func(i, j int) { nodes[i], nodes[j] = nodes[j], nodes[i] })
+	rand.Shuffle(len(memoryNodes), func(i, j int) {
+		memoryNodes[i], memoryNodes[j] = memoryNodes[j], memoryNodes[i]
+	})
 
-	return &nodes, err
+	return &memoryNodes
 }
 
-func (r *NodeRepo) getRoundRobinNodes() (*[]models.Node, error) {
-	var nodes []models.Node
+func (r *NodeRepo) getRoundRobinNodes() *[]models.Node {
+	sort.Slice(memoryNodes[:], func(i, j int) bool {
+		return memoryNodes[i].LastUsed < memoryNodes[j].LastUsed
+	})
 
-	q := r.db.Select(q.Lte("Cooldown", 0))
-	err := q.OrderBy("LastUsed").Find(&nodes)
-
-	return &nodes, err
+	return &memoryNodes
 }
 
-func (r *NodeRepo) GetActiveNodes(selection string) (*[]models.Node, error) {
+func (r *NodeRepo) GetActiveNodes(selection string) *[]models.Node {
 	if selection == "round-robin" {
 		return r.getRoundRobinNodes()
 	}
@@ -71,15 +81,43 @@ func (r *NodeRepo) GetActiveNodes(selection string) (*[]models.Node, error) {
 	return r.getRandomNodes()
 }
 
-func (r *NodeRepo) PenalizeNode(node *models.Node) {
-	node.LastUsed = time.Now().Unix()
-	err := r.db.Update(node)
-	if err != nil {
-		log.Errorf("Failed saving node request because of: %v", err)
+func (r *NodeRepo) updateMemoryLastUsedTime(targetNode *models.Node) error {
+	for i, node := range memoryNodes {
+		if targetNode.ID == node.ID {
+			tempNode := &memoryNodes[i]
+			tempNode.LastUsed = time.Now().Unix()
+			return nil
+		}
 	}
+
+	return fmt.Errorf("No target node in memory")
+}
+
+func (r *NodeRepo) RemoveNodeFromActive(targetNode *models.Node) error {
+	for i, node := range memoryNodes {
+
+		if targetNode.ID == node.ID {
+			memoryNodes[len(memoryNodes)-1], memoryNodes[i] = memoryNodes[i], memoryNodes[len(memoryNodes)-1]
+			memoryNodes = memoryNodes[:len(memoryNodes)-1]
+			return nil
+		}
+
+	}
+
+	return fmt.Errorf("No target node in memory")
+}
+
+func (r *NodeRepo) AddNodeToActive(node *models.Node) {
+	memoryNodes = append(memoryNodes, *node)
+}
+
+func (r *NodeRepo) PenalizeNode(node *models.Node) {
+	_ = r.RemoveNodeFromActive(node)
 }
 
 func (r *NodeRepo) RewardNode(node *models.Node) {
+	_ = r.updateMemoryLastUsedTime(node)
+
 	node.LastUsed = time.Now().Unix()
 	err := r.db.Update(node)
 	if err != nil {
