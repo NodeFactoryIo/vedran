eaa36d1be944b4991574599296286bfc9108c597
Matija Petrunic <matija.petrunic@fer.hr>
2020-10-12 14:59:38 +0200

    Fix ping repository

--- internal/repositories/ping.go
+++ internal/repositories/ping.go
@@ -18,22 +18,22 @@ func NewPingRepo(db *storm.DB) *PingRepo {
 }
 
 func (r *PingRepo) FindByNodeID(nodeId string) (*models.Ping, error) {
-	var ping *models.Ping
-	err := r.db.One("NodeId", nodeId, ping)
-	return ping, err
+	var ping models.Ping
+	err := r.db.One("NodeId", nodeId, &ping)
+	return &ping, err
 }
 
 func (r *PingRepo) Save(ping *models.Ping) error {
 	return r.db.Save(ping)
 }
 
-func (r PingRepo) GetAll() (*[]models.Ping, error) {
+func (r *PingRepo) GetAll() (*[]models.Ping, error) {
 	var pings []models.Ping
 	err := r.db.All(&pings)
 	return &pings, err
 }
 
-func (r PingRepo) CalculateDowntime(nodeId string, pingTime time.Time) (time.Time, time.Duration, error) {
+func (r *PingRepo) CalculateDowntime(nodeId string, pingTime time.Time) (time.Time, time.Duration, error) {
 	lastPing, err := r.FindByNodeID(nodeId)
 	if err != nil {
 		return pingTime, time.Duration(0), err
