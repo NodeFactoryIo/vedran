76fc8c2d660008548beb4aea15c6e90e0d7b5b8d
Matija Petrunic <matija.petrunic@fer.hr>
2020-10-13 12:22:36 +0200

    Avoid panic if no nodes in database

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"log"
+
 	"github.com/NodeFactoryIo/vedran/internal/controllers"
 	"github.com/NodeFactoryIo/vedran/internal/repositories"
 	"github.com/asdine/storm/v3"
@@ -14,7 +16,7 @@ func CreateNewApiRouter(db *storm.DB, whitelistEnabled bool) *mux.Router {
 	nodeRepo := repositories.NewNodeRepo(db)
 	err := nodeRepo.InitNodeRepo()
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed initializing node repo because of: %v", err)
 	}
 	pingRepo := repositories.NewPingRepo(db)
 	metricsRepo := repositories.NewMetricsRepo(db)
