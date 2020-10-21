060c08f180ca763f4a4530994386c737ec1a4660
Mak Muftic <mak@nodefactory.io>
2020-10-21 13:20:24 +0200

    Restructure code

--- internal/whitelist/whitelist.go
+++ internal/whitelist/whitelist.go
@@ -14,15 +14,16 @@ var (
 	whitelistedNodes []string
 )
 
+var newLine = []byte{'\n'}
+
 func IsNodeWhitelisted(nodeId string) bool {
 	if fileWithWhitelistedNodes != "" {
-		// read whitelisted from file
 		file, err := ioutil.ReadFile(fileWithWhitelistedNodes)
 		if err != nil {
 			log.Errorf("Unable to read file with whitelisted nodes %s because %v", fileWithWhitelistedNodes, err)
 			return false
 		}
-		for _, nodeIdBytes := range bytes.Split(file, []byte{'\n'}) {
+		for _, nodeIdBytes := range bytes.Split(file, newLine) {
 			if string(nodeIdBytes) == nodeId {
 				return true
 			}
@@ -49,6 +50,57 @@ func InitWhitelisting(whitelistedNodes []string, whitelistFile string) (bool, er
 		whitelistEnabled = false
 	}
 	return whitelistEnabled, whitelistError
+}
+
+func RemoveNodeFromWhitelisted(nodeId string) error {
+	if fileWithWhitelistedNodes != "" {
+		return removeNodeFromWhitelistFile(nodeId)
+	} else {
+		return removeNodeFromWhitelistArray(nodeId)
+	}
+}
+
+func removeNodeFromWhitelistFile(nodeId string) error {
+	file, err := ioutil.ReadFile(fileWithWhitelistedNodes)
+	if err != nil {
+		return err
+	}
+	// remove node id from file content
+	var newFileContent []byte
+	lines := bytes.Split(file, newLine)
+	for i, nodeIdBytes := range lines {
+		if string(nodeIdBytes) != nodeId {
+			newFileContent = append(newFileContent, nodeIdBytes...)
+			if i + 1 < len(lines) { // append new line expect last entry
+				newFileContent = append(newFileContent, newLine...)
+			}
+		}
+	}
+	// save new file content to file
+	err = ioutil.WriteFile(fileWithWhitelistedNodes, newFileContent, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func removeNodeFromWhitelistArray(nodeId string) error {
+	// find node id inside array
+	removeIndex := -1
+	for i, id := range whitelistedNodes {
+		if id == nodeId {
+			removeIndex = i
+		}
+	}
+	// remove node if index found
+	if removeIndex != -1 {
+		whitelistedNodes[removeIndex] = whitelistedNodes[len(whitelistedNodes) - 1]
+		whitelistedNodes[len(whitelistedNodes) - 1] = ""
+		whitelistedNodes = whitelistedNodes[:len(whitelistedNodes) - 1]
+	} else {
+		return fmt.Errorf("node %s not found in whitelisted nodes", nodeId)
+	}
+	return nil
 }
 
 func initWhitelistedNodes(nodes []string) error {
--- internal/whitelist/whitelist_test.go
+++ internal/whitelist/whitelist_test.go
@@ -2,15 +2,16 @@ package whitelist
 
 import (
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
 // helper functions
 
-func createTmpWhitelistTestFile(t *testing.T) {
+func createTmpWhitelistTestFile(t *testing.T, fileContent string) {
 	file, _ := os.Create("./tmp_whitelist_test.txt")
-	_, err := file.WriteString("node1\nnode2\nnode3")
+	_, err := file.WriteString(fileContent)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -23,12 +24,97 @@ func reset() {
 
 // tests
 
+func Test_RemoveNodeFromWhitelisted_FromMemory(t *testing.T) {
+	tests := []struct {
+		name                        string
+		whitelistedNodes            []string
+		removeNodeId                string
+		removeFails                 bool
+		whitelistedNodesAfterRemove []string
+	}{
+		{
+			name:                        "remove one whitelisted node",
+			whitelistedNodes:            []string{"node1", "node2", "node3"},
+			removeNodeId:                "node2",
+			removeFails:                 false,
+			whitelistedNodesAfterRemove: []string{"node1", "node3"},
+		},
+		{
+			name:                        "fail on removing not whitelisted node",
+			whitelistedNodes:            []string{"node1", "node2", "node3"},
+			removeNodeId:                "node4",
+			removeFails:                 true,
+			whitelistedNodesAfterRemove: []string{"node1", "node3"},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_ = initWhitelistedNodes(test.whitelistedNodes)
+			err := RemoveNodeFromWhitelisted(test.removeNodeId)
+			if test.removeFails {
+				assert.Error(t, err)
+				assert.Equal(t, test.whitelistedNodes, whitelistedNodes)
+			} else {
+				assert.Nil(t, err)
+				assert.Equal(t, test.whitelistedNodesAfterRemove, whitelistedNodes)
+			}
+			reset()
+		})
+	}
+}
+
+func Test_RemoveNodeFromWhitelisted_FromFile(t *testing.T) {
+	tests := []struct {
+		name                       string
+		removeNodeId               string
+		removeFails                bool
+		whitelistedFile            string
+		whitelistedFileAfterRemove string
+	}{
+		{
+			name:                       "remove one whitelisted node",
+			whitelistedFile:            "node1\nnode2\nnode3",
+			whitelistedFileAfterRemove: "node1\nnode3",
+			removeNodeId:               "node2",
+			removeFails:                false,
+		},
+		{
+			name:                       "remove one whitelisted node",
+			whitelistedFile:            "node1\nnode2\nnode3",
+			whitelistedFileAfterRemove: "node1\nnode2\nnode3",
+			removeNodeId:               "node4",
+			removeFails:                false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			createTmpWhitelistTestFile(t, test.whitelistedFile)
+			defer os.Remove("./tmp_whitelist_test.txt")
+
+			_ = initWhitelistedNodesFromFile("./tmp_whitelist_test.txt")
+
+			err := RemoveNodeFromWhitelisted(test.removeNodeId)
+
+			fileContent, _ := ioutil.ReadFile("./tmp_whitelist_test.txt")
+			fileContentString := string(fileContent)
+			if test.removeFails {
+				assert.Error(t, err)
+				assert.Equal(t, test.whitelistedFile, fileContentString)
+			} else {
+				assert.Nil(t, err)
+				assert.Equal(t, test.whitelistedFileAfterRemove, fileContentString)
+			}
+			reset()
+		})
+	}
+}
+
 func Test_WhitelistingFromMemory(t *testing.T) {
 	_ = initWhitelistedNodes([]string{"node1", "node2", "node3"})
 
 	tests := []struct {
-		name string
-		nodeID string
+		name     string
+		nodeID   string
 		expected bool
 	}{
 		{name: "whitelisted node1", nodeID: "node1", expected: true},
@@ -47,14 +133,14 @@ func Test_WhitelistingFromMemory(t *testing.T) {
 }
 
 func Test_WhitelistingFromFile(t *testing.T) {
-	createTmpWhitelistTestFile(t)
+	createTmpWhitelistTestFile(t, "node1\nnode2\nnode3")
 	defer os.Remove("./tmp_whitelist_test.txt")
 
 	_ = initWhitelistedNodesFromFile("./tmp_whitelist_test.txt")
 
 	tests := []struct {
-		name string
-		nodeID string
+		name     string
+		nodeID   string
 		expected bool
 	}{
 		{name: "whitelisted node1", nodeID: "node1", expected: true},
@@ -73,7 +159,7 @@ func Test_WhitelistingFromFile(t *testing.T) {
 }
 
 func Test_WhitelistInit(t *testing.T) {
-	createTmpWhitelistTestFile(t)
+	createTmpWhitelistTestFile(t, "node1\nnode2\nnode3")
 	defer os.Remove("./tmp_whitelist_test.txt")
 
 	assert.Nil(t, initWhitelistedNodesFromFile("./tmp_whitelist_test.txt"))
@@ -91,5 +177,3 @@ func Test_InitWhitelistedNodesFromFile_FileDoesntExist(t *testing.T) {
 	assert.Error(t, initWhitelistedNodesFromFile("test-file.txt"))
 	reset()
 }
-
-
