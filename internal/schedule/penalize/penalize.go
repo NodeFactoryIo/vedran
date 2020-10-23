6561c842b44b1743f3208a4b01c957dc618dae1d
Mak Muftic <mak@nodefactory.io>
2020-10-23 11:06:49 +0200

    Fix typo

--- internal/schedule/penalize/penalize.go
+++ internal/schedule/penalize/penalize.go
@@ -45,7 +45,7 @@ func ScheduleCheckForPenalizedNode(node models.Node, repositories repositories.R
 				}
 				return
 			}
-			log.Debugf("Node %s still not active, on cooldown for %d minute ", node.ID, nodeWithNewCooldown.Cooldown)
+			log.Debugf("Node %s is still not active, on new cooldown for %d minute ", node.ID, nodeWithNewCooldown.Cooldown)
 			ScheduleCheckForPenalizedNode(*nodeWithNewCooldown, repositories)
 		}
 	})
