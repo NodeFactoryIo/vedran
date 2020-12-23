cdd0a84f7fb5005f5765814b45b04cf119783e6b
Mak Muftic <mak@nodefactory.io>
2020-12-23 13:45:12 +0100

    Restructure payout code

--- internal/controllers/stats.go
+++ internal/controllers/stats.go
@@ -2,13 +2,14 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/NodeFactoryIo/vedran/internal/configuration"
 	"github.com/NodeFactoryIo/vedran/internal/models"
-	"github.com/NodeFactoryIo/vedran/internal/payout"
 	"github.com/NodeFactoryIo/vedran/internal/stats"
 
 	"github.com/NodeFactoryIo/go-substrate-rpc-client/signature"
@@ -23,8 +24,10 @@ type StatsResponse struct {
 	Stats map[string]models.NodeStatsDetails `json:"stats"`
 }
 
+// handler for `GET /api/v1/stats`
 func (c *ApiController) StatisticsHandlerAllStats(w http.ResponseWriter, r *http.Request) {
-	statistics, err := payout.GetStatsForPayout(c.repositories, getNow(), false)
+	timestamp := getNow()
+	statistics, err := stats.CalculateStatisticsFromLastPayout(c.repositories, timestamp)
 	if err != nil {
 		log.Errorf("Failed to calculate statistics, because %v", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
@@ -42,21 +45,15 @@ type LoadbalancerStatsResponse struct {
 	Fee   float32                            `json:"fee"`
 }
 
+type LoadbalancerStatsRequest struct {
+	TotalReward string `json:"total_reward"`
+}
+
+// handler for `POST /api/v1/stats`
 func (c *ApiController) StatisticsHandlerAllStatsForLoadbalancer(w http.ResponseWriter, r *http.Request) {
-	sig := r.Header.Get("X-Signature")
-	if sig == "" {
-		log.Error("Missing signature header")
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-	}
-	sigInBytes, err := hexutil.Decode(sig)
+	verified, httpStatusCode, err := verifySignatureInHeader(r, c.privateKey)
 	if err != nil {
-		log.Errorf("Unable to decode signature, because of: %v", err)
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-	}
-	verified, err := signature.Verify([]byte(StatsSignedData), sigInBytes, c.privateKey)
-	if err != nil {
-		log.Errorf("Failed to verify signature, because %v", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		http.Error(w, http.StatusText(httpStatusCode), httpStatusCode)
 		return
 	}
 	if !verified {
@@ -65,13 +62,31 @@ func (c *ApiController) StatisticsHandlerAllStatsForLoadbalancer(w http.Response
 		return
 	}
 
-	statistics, err := payout.GetStatsForPayout(c.repositories, getNow(), true)
+	totalRewardAsFloat, err := getTotalRewardFromRequest(r)
+	if err != nil {
+		log.Error(err)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	}
+
+	timestamp := getNow()
+	statistics, err := stats.CalculateStatisticsFromLastPayout(c.repositories, timestamp)
 	if err != nil {
 		log.Errorf("Failed to calculate statistics, because %v", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
+	err = c.repositories.PayoutRepo.Save(&models.Payout{
+		Timestamp:      timestamp,
+		PaymentDetails: statistics,
+		LbFee:          totalRewardAsFloat * float64(configuration.Config.Fee),
+	})
+	if err != nil {
+		log.Errorf("Failed to save payout, because %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(LoadbalancerStatsResponse{
 		Stats: statistics,
@@ -79,6 +94,43 @@ func (c *ApiController) StatisticsHandlerAllStatsForLoadbalancer(w http.Response
 	})
 }
 
+func verifySignatureInHeader(r *http.Request, privateKey string) (bool, int, error) {
+	sig := r.Header.Get("X-Signature")
+	if sig == "" {
+		log.Error("Missing signature header")
+		return false, http.StatusBadRequest, nil
+	}
+	sigInBytes, err := hexutil.Decode(sig)
+	if err != nil {
+		log.Errorf("Unable to decode signature, because of: %v", err)
+		return false, http.StatusBadRequest, err
+	}
+	verified, err := signature.Verify([]byte(StatsSignedData), sigInBytes, privateKey)
+	if err != nil {
+		log.Errorf("Failed to verify signature, because %v", err)
+		return false, http.StatusInternalServerError, err
+	}
+	return verified, 0, err
+}
+
+func getTotalRewardFromRequest(r *http.Request) (float64, error) {
+	var statsRequest LoadbalancerStatsRequest
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return 0, err
+	}
+	err = json.Unmarshal(reqBody, &statsRequest)
+	if err != nil {
+		return 0, fmt.Errorf("invalid request body: %v", err)
+	}
+	totalRewardAsFloat, err := strconv.ParseFloat(statsRequest.TotalReward, 64)
+	if err != nil {
+		return 0, fmt.Errorf("invalid total reward value: %v", err)
+	}
+	return totalRewardAsFloat, nil
+}
+
+// handler for `GET /api/v1/stats/node/{id}`
 func (c *ApiController) StatisticsHandlerStatsForNode(w http.ResponseWriter, r *http.Request) {
 	vars := muxhelpper.Vars(r)
 	nodeId, ok := vars["id"]
@@ -108,6 +160,7 @@ type LbStatsResponse struct {
 	NodeFee string `json:"nodes_fee"`
 }
 
+// handler for `GET /api/v1/stats/lb`
 func (c *ApiController) StatisticsHandlerStatsForLoadBalancer(w http.ResponseWriter, r *http.Request) {
 	statsResponse := LbStatsResponse{
 		LbFee:   strconv.FormatFloat(float64(configuration.Config.Fee), 'f', -1, 32),
