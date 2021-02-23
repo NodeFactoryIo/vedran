a52a1c0592999b1361527350c9cc8833785d2e42
Matija Petrunic <matija.petrunic@fer.hr>
2021-02-23 10:33:23 +0100

    Remove transfer channel

--- pkg/http-tunnel/server/tcpproxy.go
+++ pkg/http-tunnel/server/tcpproxy.go
@@ -81,7 +81,6 @@ func (p *TCPProxy) Proxy(w io.Writer, r io.ReadCloser, msg *proto.ControlMessage
 		}).Error("dial failed", err)
 		return
 	}
-	defer local.Close()
 
 	if err := tunnel.KeepAlive(local); err != nil {
 		clogger.WithFields(log.Fields{
@@ -89,7 +88,6 @@ func (p *TCPProxy) Proxy(w io.Writer, r io.ReadCloser, msg *proto.ControlMessage
 		}).Error("TCP keepalive for tunneled connection failed", err)
 	}
 
-	done := make(chan struct{})
 	go func() {
 		loggerWithContext := log.WithContext(p.logger.Context).WithFields(log.Fields{
 			"dst": msg.ForwardedHost,
@@ -97,7 +95,6 @@ func (p *TCPProxy) Proxy(w io.Writer, r io.ReadCloser, msg *proto.ControlMessage
 		})
 		transfer(flushWriter{w}, local, loggerWithContext)
 		clogger.Info("Transfer close called")
-		close(done)
 	}()
 
 	loggerWithContext := log.WithContext(p.logger.Context).WithFields(log.Fields{
@@ -105,15 +102,10 @@ func (p *TCPProxy) Proxy(w io.Writer, r io.ReadCloser, msg *proto.ControlMessage
 		"src": target,
 	})
 	transfer(local, r, loggerWithContext)
-	close(done)
-
-	<-done
-	/*
-		err = local.Close()
-		if err != nil {
-			clogger.Errorf("Local close failed because of %v")
-		}
-	*/
+	err = local.Close()
+	if err != nil
+		clogger.Errorf("Local close failed because of %v")
+	}
 
 	clogger.Info("Transfer close called and magic happeneded")
 }
